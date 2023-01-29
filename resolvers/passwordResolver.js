e05cbf5f07e67be30cb0b1d53264861b6b31523b
P4ul0Silva <paulors43@outlook.com>
2023-01-29 14:45:23 -0300

    feat: added minUpperCase + minLowerCase rules

--- resolvers/passwordResolver.js
+++ resolvers/passwordResolver.js
@@ -3,6 +3,25 @@ const validators = {
   minSize: (rule, password) => {
     return password.length >= Number(rule.value);
   },
+
+  minUpperCase: (rule, password) => {
+    const match = password.match(/[A-Z\u00C0-\u017F]/g);
+
+    if (match) {
+      return match.length >= Number(rule.value);
+    }
+    return false;
+  },
+
+  minLowerCase: (rule, password) => {
+    const match = password.match(/[a-z\u00C0-\u017F]/g);
+
+    if (match) {
+      return match.length >= Number(rule.value);
+    }
+    return false;
+  },
+
   minSpecialChars: (rule, password) => {
     const match = password.match(/[!@#\$%\^&\*\(\)\-+\/\{\}\[\]]/g);
 
